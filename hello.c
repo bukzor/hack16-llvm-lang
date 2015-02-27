c53b9efb40676e00a836a84c3037a55441b23b46
Buck Evan <buck.2019@gmail.com>
2015-02-27 15:11:28 -0800

    separate eval and interpret functions

--- hello.c
+++ hello.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+const char *source;
+int pos;
 
 
-int main() {
+void _eval(int (*getc)()) {
     int c;
-
-    c = getchar();
+    c = getc();
     while(c != EOF) {
         if(c == 'h') {
-            c = getchar();
+            c = getc();
             if(c == 'e') {
-                c = getchar();
+                c = getc();
                 if(c == 'l') {
-                    c = getchar();
+                    c = getc();
                     if(c == 'l') {
-                        c = getchar();
+                        c = getc();
                         if(c == 'o') {
                             printf("Hello, world!\n");
                         }
@@ -21,10 +25,39 @@ int main() {
                 }
             }
         } else {
-            c = getchar();
+            c = getc();
         }
     }
+}
 
+
+int sgetc() {
+    if (pos == EOF) {
+        return EOF;
+    }
+
+    int c = (int)source[pos];
+    if (c == 0) {
+        pos = EOF;
+    } else {
+        pos += 1;
+    }
+    return c;
+}
+
+
+static void eval(const char* _source) {
+    source = _source;
+    pos = 0;
+    _eval(sgetc);
+}
+
+void interpreter() {
+    _eval(getc);
+}
+
+int main() {
+    eval("hello\nhello\nno\n");
     return 0;
 }
 
